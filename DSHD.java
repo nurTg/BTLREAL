6fca2fa71029a2ce4146e4264c70f10ade11737c
nurTg <144673392+nurTg@users.noreply.github.com>
2024-04-16 15:51:22 +0700

    Update DSHD.java

--- DSHD.java
+++ DSHD.java
@@ -1,3 +1,8 @@
+package BTL;
+
+import BTL.Khachhang;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -26,4 +31,59 @@ public class DSHD {
             hoadon.hienHD();
         }
     }
+    public void timkiemHD() {
+        int ma ;
+        sc.nextLine();
+        System.out.print("\nNhập mã hóa đơn cần tìm: ");
+        ma = sc.nextInt() ;
+        System.out.println("\n----THÔNG TIN HÓA ĐƠN CẦN TÌM ----");
+        for (Hoadon hoadon: dshd) {
+            if (hoadon.getMaHD()==ma) {
+                hienDSHD();
+            }
+            else {
+                System.out.println("Không có mã hóa đơn cần tìm");
+            }
+        }
+    }
+    public void tongtien() {
+        float tongtien = 0;
+        for(Hoadon hoadon:dshd) {
+            tongtien += hoadon.getThanhTien();
+        }
+        System.out.printf("\n\tTổng tiền của các hóa đơn: %.1f" ,tongtien);
+    }
+    public void ghifile() throws IOException {
+        try {
+            FileOutputStream fos = new FileOutputStream("DSHD.DAT");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(dshd);
+            oos.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("\nGhi File thất bại!"+e.toString());
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void docfile() throws IOException {
+        try {
+            FileInputStream fis = new FileInputStream("DSHD.DAT");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            ArrayList<Hoadon> ds = (ArrayList<Hoadon>) ois.readObject();
+            ois.close();
+            fis.close();
+            dshd.clear();
+            dshd=ds;
+            hienDSHD();
+        }
+        catch (ClassNotFoundException | FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("\nĐọc File thất bại!"+e.toString());
+        }
+    }
+
 }
